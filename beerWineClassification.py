13d7072afc8366b62ae7c3e05e0d8e531bbdf19b
Tarun Maini <mainitarun7@gmail.com>
2020-09-12 01:18:39 +0530

    Updating feature values

--- beerWineClassification.py
+++ beerWineClassification.py
@@ -1,15 +1,11 @@
-#🥃 🍾 🍻 🥂 🍺 🍷 🥃 🍾 🍻 🥂 🍺 🍷🍷🍷🍷 🥃 🍾 🍻 🥂 🍺 🍷 🥃 🍾 🍻 🥂
-
 # Beer Vs. Wine
 # Classification based on Alcoholic Content & Color
 
 from sklearn import tree
 #features = [[4,"amber"], [5,"amber"], [10,"red"], [11.5,"red"]]
-features = [[4, 0], [5, 0], [10, 1], [11.5, 1]]
+features = [[4, 0], [5, 0], [9, 1],[12, 1]]
 # labels = ["beer","beer","wine","wine"]
 labels = [0, 0, 1, 1]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
-print(clf.predict([[9, 1]]))
-
-#🍺 🍷 🥃 🍾 🍻 🥂 🍺 🍷 🥃 🍾 🍻 🥂 🍺 🍷 🥃 🍾 🍻 🥂 🍺 🍷 🥃 🍾 🍻 🥂
+print(clf.predict([[8, 1]]))
